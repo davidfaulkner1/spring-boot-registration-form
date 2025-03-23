50db8610c7af4610ba772bcc799c8a198c078122
David Faulkner <68970573+davidfaulkner1@users.noreply.github.com>
2025-03-23 15:37:13 +0000

    Update CustomerDataRepository.java formatting changes

--- src/main/java/com/davidfaulkner/registration/spring_boot_registration_form/repository/CustomerDataRepository.java
+++ src/main/java/com/davidfaulkner/registration/spring_boot_registration_form/repository/CustomerDataRepository.java
@@ -1,11 +1,7 @@
-/**
- * 
- */
 package com.davidfaulkner.registration.spring_boot_registration_form.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.davidfaulkner.registration.spring_boot_registration_form.entity.CustomerData;
 
 /**
